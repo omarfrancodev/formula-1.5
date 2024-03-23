ef1362638542df2dd00d3797391f66a4e1a70134
Omar Franco <fofe2803@gmail.com>
2024-03-22 19:59:45 -0600

    refactor: changes logic of saving scores, sorting and code removal
    update method of saving scores, improve sorting function, remove unnecessary code.

--- src/scripts/F1CalculatedStandings.js
+++ src/scripts/F1CalculatedStandings.js
@@ -1,12 +1,14 @@
 export function updateStandings(data, driverStandings, constructorsStandings) {
   data.raceStandings.forEach((result) => {
-    const existingDriver = driverStandings.find((d) => d.Driver.driverId === result.Driver.driverId)
+    const existingDriver = driverStandings.find((d) => d.driverId === result.Driver.driverId)
     if (existingDriver) {
       existingDriver.Points += result.points
     } else {
       driverStandings.push({
-        Driver: result.Driver,
-        Car: result.Constructor.name,
+        driverId: result.Driver.driverId,
+        Name: `${result.Driver.givenName} ${result.Driver.familyName}`,
+        Nationality: `${result.Driver.nationality}`,
+        Constructor: result.Constructor.name,
         Points: result.points
       })
     }
@@ -14,66 +16,24 @@ export function updateStandings(data, driverStandings, constructorsStandings) {
 
   data.raceStandings.forEach((result) => {
     const existingConstructor = constructorsStandings.find(
-      (c) => c.Constructor.constructorId === result.Constructor.constructorId
+      (c) => c.constructorId === result.Constructor.constructorId
     )
     if (existingConstructor) {
       existingConstructor.Points += result.points
     } else {
       constructorsStandings.push({
-        Constructor: result.Constructor,
+        constructorId: result.Constructor.constructorId,
+        Name: `${result.Constructor.name}`,
         Points: result.points
       })
     }
   })
 }
 
-function orderByPoints(standings) {
-  return standings.sort((a, b) => b.Points - a.Points)
-}
-
-export async function showStandingsOnHTML(driverStandings, constructorsStandings) {
-  driverStandings = orderByPoints(driverStandings)
-  constructorsStandings = orderByPoints(constructorsStandings)
-
-  createTable('Drivers', driverStandings, true)
-  createTable('Constructors', constructorsStandings, false)
-}
-
-function createTable(title, standings, isDriver) {
-  const resultDiv = document.getElementById('result')
-
-  const titleTable = document.createElement('h2')
-  titleTable.textContent = `${title} Standings`
-  resultDiv.appendChild(titleTable)
-
-  const table = document.createElement('table')
-
-  const headerRow = table.insertRow()
-
-  let headers = []
-  if (isDriver) {
-    headers = ['Position', 'Name', 'Nationality', 'Car', 'Points']
-  } else {
-    headers = ['Position', 'Team', 'Points']
-  }
-  headers.forEach((headerText) => {
-    const header = document.createElement('th')
-    header.textContent = headerText
-    headerRow.appendChild(header)
+export function orderByPoints(standings) {
+  let aux = standings.sort((a, b) => b.Points - a.Points)
+  aux.forEach((result, index) => {
+    result.Position = index + 1
   })
-
-  standings.forEach((data, index) => {
-    const row = table.insertRow()
-    row.insertCell().textContent = index + 1
-    if (isDriver) {
-      row.insertCell().textContent = `${data.Driver.givenName} ${data.Driver.familyName}`
-      row.insertCell().textContent = `${data.Driver.nationality}`
-      row.insertCell().textContent = data.Car
-    } else {
-      row.insertCell().textContent = `${data.Constructor.name}`
-    }
-    row.insertCell().textContent = data.Points
-  })
-
-  resultDiv.appendChild(table)
+  return aux
 }
